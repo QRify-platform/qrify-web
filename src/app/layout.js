d84f4df0e4afd76fc428c7a76d32981bd872f207
bryanpow <ramosbusiness544@gmail.com>
2025-06-19 20:17:19 -0400

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,25 @@
-import './globals.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { usePathname } from 'next/navigation'
 export const metadata = {
-  title: 'QRify – Instant QR Code Generator',
-  description: 'A clean SaaS app to generate QR codes from URLs.',
+  title: "QRify – Instant QR Code Generator",
+  description: "A clean SaaS app to generate QR codes from URLs.",
 };
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    fetch("/track-metric", {
+      method: "POST",
+      body: JSON.stringify({ metric: "page_load", path: pathname }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }, [pathname]);
   return (
     <html lang="en">
-      <body  style={{color: '#1f2937' }}>
+      <body style={{ color: "#1f2937" }}>
         <Navbar />
         {children}
         <Footer />
